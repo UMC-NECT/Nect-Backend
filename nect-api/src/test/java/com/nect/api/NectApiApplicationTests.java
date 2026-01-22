45e3a110b3ea17ef05c1fc2319009962fa49f33a
junyong <rlawnsdyd530@gmail.com>
2026-01-23 04:56:29 +0900

    [Fix] contextLoads 테스트 제거

--- nect-api/src/test/java/com/nect/api/NectApiApplicationTests.java
+++ nect-api/src/test/java/com/nect/api/NectApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.nect.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NectApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
