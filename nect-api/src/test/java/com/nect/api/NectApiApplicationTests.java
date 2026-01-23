2213052c5d4b3d1770861e0801a876969bcce102
junyong <rlawnsdyd530@gmail.com>
2026-01-23 16:21:43 +0900

    [Fix] ProcessRepository에서 실제 Process 엔티티 클래스를 사용하지 않아 발생하는 테스트오류 해결

--- nect-api/src/test/java/com/nect/api/NectApiApplicationTests.java
+++ nect-api/src/test/java/com/nect/api/NectApiApplicationTests.java
@@ -1,8 +1,13 @@
 package com.nect.api;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
