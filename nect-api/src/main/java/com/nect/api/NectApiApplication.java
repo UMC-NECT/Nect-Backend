a892700a9199f4b2b4c991df4944894a4bf54a5f
kjunh972 <kjunh972@gmail.com>
2026-01-27 02:39:32 +0900

    Update: 충돌 수정

--- nect-api/src/main/java/com/nect/api/NectApiApplication.java
+++ nect-api/src/main/java/com/nect/api/NectApiApplication.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.nect")
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "com.nect")
 @EntityScan(basePackages = "com.nect")
