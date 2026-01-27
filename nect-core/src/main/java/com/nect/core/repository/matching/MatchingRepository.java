21b269fa4cec5616c27402f5e3b7a8f57ee5ad75
zeoueon <zeoueon129@gmail.com>
2026-01-27 16:26:31 +0900

    [Feat] 매칭 요청 조회를 위한 Repository 함수 추가

--- nect-core/src/main/java/com/nect/core/repository/matching/MatchingRepository.java
+++ nect-core/src/main/java/com/nect/core/repository/matching/MatchingRepository.java
@@ -1,7 +1,47 @@
 package com.nect.core.repository.matching;
 
 import com.nect.core.entity.matching.Matching;
+import com.nect.core.entity.matching.enums.MatchingRequestType;
+import com.nect.core.entity.matching.enums.MatchingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface MatchingRepository extends JpaRepository<Matching, Long> {
+
+    int countByRequestTypeAndRequestUserIdAndMatchingStatus(
+            MatchingRequestType requestType,
+            Long requestUserId,
+            MatchingStatus status
+    );
+
+    int countByRequestTypeAndProjectIdAndMatchingStatus(
+            MatchingRequestType requestType,
+            Long projectId,
+            MatchingStatus status
+    );
+
+    // SENT
+    List<Matching> findByRequestUserIdAndMatchingStatusOrderByExpiresAtAsc(
+            Long requestUserId,
+            MatchingStatus matchingStatus
+    );
+
+    List<Matching> findByRequestUserIdAndMatchingStatusInOrderByCreatedAtDesc(
+            Long requestUserId,
+            List<MatchingStatus> statuses
+    );
+
+    //RECEIVED
+    List<Matching> findByTargetUserIdAndMatchingStatusOrderByExpiresAtAsc(
+            Long targetUserId,
+            MatchingStatus matchingStatus
+    );
+
+    List<Matching> findByTargetUserIdAndMatchingStatusInOrderByCreatedAtDesc(
+            Long targetUserId,
+            List<MatchingStatus> statuses
+    );
 }
