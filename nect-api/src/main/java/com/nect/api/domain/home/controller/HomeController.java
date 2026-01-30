0e6b393f04761e093ff67329cd5bc3c5c1469370
junyong <rlawnsdyd530@gmail.com>
2026-01-30 21:49:18 +0900

    [Refactor] 인증헤더 넣도록 변경

--- nect-api/src/main/java/com/nect/api/domain/home/controller/HomeController.java
+++ nect-api/src/main/java/com/nect/api/domain/home/controller/HomeController.java
@@ -5,7 +5,9 @@ import com.nect.api.domain.home.dto.HomeProjectResponse;
 import com.nect.api.domain.home.service.HomeQueryService;
 import com.nect.api.domain.home.service.HomeRecommendService;
 import com.nect.api.global.response.ApiResponse;
+import com.nect.api.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,29 +23,29 @@ public class HomeController {
 
     // 모집 중인 프로젝트 조회
     @GetMapping("/projects")
-    public ApiResponse<HomeProjectResponse> recruitingProjects(@RequestParam("count") int count){
-        HomeProjectResponse projects = queryService.getProjects(1L, count);
+    public ApiResponse<HomeProjectResponse> recruitingProjects(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam("count") int count){
+        HomeProjectResponse projects = queryService.getProjects(userDetails.getUserId(), count);
         return ApiResponse.ok(projects);
     }
 
     // 홈화면 프로젝트 추천
     @GetMapping("/recommendations/projects")
-    public ApiResponse<HomeProjectResponse> recommendedProjects(@RequestParam("count") int count){
-        HomeProjectResponse projects = recommendService.getProjects(1L, count);
+    public ApiResponse<HomeProjectResponse> recommendedProjects(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam("count") int count){
+        HomeProjectResponse projects = recommendService.getProjects(userDetails.getUserId(), count);
         return ApiResponse.ok(projects);
     }
 
     // 홈화면 매칭 가능한 넥터
     @GetMapping("/members")
-    public ApiResponse<HomeMembersResponse> matchableMembers(@RequestParam("count") int count){
-        HomeMembersResponse members = queryService.getMembers(1L, count);
+    public ApiResponse<HomeMembersResponse> matchableMembers(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam("count") int count){
+        HomeMembersResponse members = queryService.getMembers(userDetails.getUserId(), count);
         return ApiResponse.ok(members);
     }
 
     // 홈화면 팀원 추천
     @GetMapping("/recommendations/members")
-    public ApiResponse<HomeMembersResponse> recommendedMembers(@RequestParam("count") int count){
-        HomeMembersResponse members = recommendService.getMembers(1L, count);
+    public ApiResponse<HomeMembersResponse> recommendedMembers(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestParam("count") int count){
+        HomeMembersResponse members = recommendService.getMembers(userDetails.getUserId(), count);
         return ApiResponse.ok(members);
     }
 
