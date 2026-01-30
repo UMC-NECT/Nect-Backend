ae3ccfebfe4fcdf35f817f044155bb1adc3016f9
junyong <rlawnsdyd530@gmail.com>
2026-01-30 16:57:20 +0900

    [Feat] 홈화면 매칭 가능한 유저 조회 API 구현   - 추가연동 필요

--- nect-api/src/main/java/com/nect/api/domain/home/service/HomeQueryService.java
+++ nect-api/src/main/java/com/nect/api/domain/home/service/HomeQueryService.java
@@ -1,6 +1,7 @@
 package com.nect.api.domain.home.service;
 
 import com.nect.api.domain.home.dto.HomeMembersResponse;
+import com.nect.api.domain.home.dto.HomeMemberItem;
 import com.nect.api.domain.home.dto.HomeProjectItem;
 import com.nect.api.domain.home.dto.HomeProjectResponse;
 import com.nect.api.domain.home.enums.code.HomeErrorCode;
@@ -9,9 +10,11 @@ import com.nect.api.global.exception.CustomException;
 import com.nect.core.entity.matching.Recruitment;
 import com.nect.core.entity.team.Project;
 import com.nect.core.entity.team.enums.RecruitmentStatus;
+import com.nect.core.entity.user.User;
 import com.nect.core.repository.matching.RecruitmentRepository;
 import com.nect.core.repository.team.ProjectRepository;
 import com.nect.core.repository.team.ProjectUserRepository;
+import com.nect.core.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -19,10 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,7 +32,9 @@ public class HomeQueryService implements HomeService {
     private final ProjectRepository projectRepository;
     private final ProjectUserRepository projectUserRepository;
     private final RecruitmentRepository recruitmentRepository;
+    private final UserRepository userRepository;
 
+    // 모집 중인 프로젝트
     @Transactional(readOnly = true)
     public HomeProjectResponse getProjects(Long userId, Integer count){
 
@@ -109,7 +111,7 @@ public class HomeQueryService implements HomeService {
 
                     return new HomeProjectItem(
                             projectId,
-                            null,
+                            null, // TODO: 프로젝트 이미지 넣기
                             project.getTitle(),
                             authorName,
                             authorPart,
@@ -117,7 +119,7 @@ public class HomeQueryService implements HomeService {
                             leftDays,
                             maxMemberCount,
                             curMemberCount,
-                            false,
+                            false, // 스크랩 여부는 제작X
                             project.getRecruitmentStatus().name(),
                             roles
                     );
@@ -127,10 +129,36 @@ public class HomeQueryService implements HomeService {
         return new HomeProjectResponse(items);
     }
 
+    // 매칭 가능한 유저
     @Transactional(readOnly = true)
     public HomeMembersResponse getMembers(Long userId, Integer count) {
 
-        return null;
+        if (userId == null || count == null) {
+            throw new CustomException(CommonResponseCode.MISSING_REQUEST_PARAMETER_ERROR);
+        }
+
+        if (count < 1) {
+            throw new CustomException(HomeErrorCode.INVALID_HOME_COUNT);
+        }
+
+        // 본인을 제외한 홈화면 매칭 가능한 유저 목록 조회
+        PageRequest pageRequest = PageRequest.of(0, count);
+        List<User> users = userRepository.findByUserIdNot(userId, pageRequest);
+
+        List<HomeMemberItem> items = users.stream()
+                .map(user -> new HomeMemberItem(
+                        user.getUserId(),
+                        null, // TODO: imageUrl은 추후 연동
+                        user.getName(),
+                        "개발자", // part는 추후 연동
+                        "나는 바보예요", // introduction은 추후 연동
+                        "매칭 가능", // status는 추후 연동
+                        false, // 스크랩 여부는 제작X
+                        new ArrayList<>(List.of("PM", "Design")) // roles는 추후 연동
+                ))
+                .toList();
+
+        return new HomeMembersResponse(items);
     }
 
 }
