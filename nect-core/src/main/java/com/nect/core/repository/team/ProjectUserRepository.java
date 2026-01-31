3c4700ad86a15b6ae8b7786bd5aedbe429dba608
infiniment <smnoh0707@gmail.com>
2026-02-01 01:07:55 +0900

    [Refactor] 프로젝트 유저 roleField 반영
    # Conflicts:
    #	nect-core/src/main/java/com/nect/core/repository/team/ProjectUserRepository.java

--- nect-core/src/main/java/com/nect/core/repository/team/ProjectUserRepository.java
+++ nect-core/src/main/java/com/nect/core/repository/team/ProjectUserRepository.java
@@ -5,6 +5,7 @@ import com.nect.core.entity.team.ProjectUser;
 import com.nect.core.entity.team.enums.ProjectMemberStatus;
 import com.nect.core.entity.team.enums.ProjectMemberType;
 import com.nect.core.entity.user.User;
+import com.nect.core.entity.user.enums.RoleField;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,9 +33,10 @@ public interface ProjectUserRepository extends JpaRepository<ProjectUser, Long> 
         ProjectMemberStatus getMemberStatus();
     }
 
-    interface UserFieldIdsRow {
+    interface UserRoleFieldsRow {
         Long getUserId();
-        Long getFieldId();
+        RoleField getRoleField();
+        String getCustomRoleFieldName();
     }
 
     Optional<ProjectUser> findByUserIdAndProject(Long userid, Project project);
@@ -82,24 +84,27 @@ public interface ProjectUserRepository extends JpaRepository<ProjectUser, Long> 
 
 
     @Query("""
-        select pu.userId as userId, pu.fieldId as fieldId
+        select
+          pu.userId as userId,
+          pu.roleField as roleField,
+          pu.customRoleFieldName as customRoleFieldName
         from ProjectUser pu
         where pu.project.id = :projectId
           and pu.memberStatus = 'ACTIVE'
           and pu.userId in :userIds
     """)
-    List<UserFieldIdsRow> findActiveUserFieldIdsByProjectIdAndUserIds(
+    List<UserRoleFieldsRow> findActiveUserRoleFieldsByProjectIdAndUserIds(
             @Param("projectId") Long projectId,
             @Param("userIds") List<Long> userIds
     );
-           
-    long countByProjectIdAndUserIdIn(Long projectId, List<Long> userIds);
+
+    long countByProject_IdAndUserIdIn(Long projectId, List<Long> userIds);
 
     @Query("""
         SELECT
             pu.userId as userId,
             u.name as name,
-            pu.fieldId as fieldId,
+            pu.roleField as fieldId,
             pu.memberType as memberType,
             pu.memberStatus as memberStatus
         FROM ProjectUser pu
@@ -108,5 +113,40 @@ public interface ProjectUserRepository extends JpaRepository<ProjectUser, Long> 
         AND pu.memberStatus = 'ACTIVE'
     """)
     List<ProjectMemberInfo> findProjectMemberInfos(@Param("projectId") Long projectId);
+
+    @Query("""
+        SELECT COUNT(pu) > 0
+        FROM ProjectUser pu
+        WHERE pu.project.id = :projectId
+          AND pu.userId = :userId
+          AND pu.memberStatus = 'ACTIVE'
+          AND pu.memberType = 'LEADER'
+    """)
+    boolean existsActiveLeader(@Param("projectId") Long projectId, @Param("userId") Long userId);
+
+
+    interface MemberBoardRow {
+        Long getUserId();
+        String getName();
+        String getNickname();
+        RoleField getRoleField();
+        String getCustomRoleFieldName();
+        ProjectMemberType getMemberType();
+    }
+
+    @Query("""
+        SELECT 
+            pu.userId as userId,
+            u.name as name,
+            u.nickname as nickname,
+            pu.roleField as roleField,
+            pu.customRoleFieldName as customRoleFieldName,
+            pu.memberType as memberType
+        FROM ProjectUser pu
+        JOIN User u ON u.userId = pu.userId
+        WHERE pu.project.id = :projectId
+          AND pu.memberStatus = 'ACTIVE'
+    """)
+    List<MemberBoardRow> findActiveMemberBoardRows(@Param("projectId") Long projectId);
 
 }
