e71951cf1e09e8b4294464d609c60b5d8e64093e
zeoueon <zeoueon129@gmail.com>
2026-02-05 01:44:18 +0900

    [Feat] 프로젝트 멤버 파트(필드) 변경 및 강퇴 API 구현

--- nect-api/src/main/java/com/nect/api/domain/team/project/controller/ProjectUserController.java
+++ nect-api/src/main/java/com/nect/api/domain/team/project/controller/ProjectUserController.java
@@ -1,14 +1,17 @@
 package com.nect.api.domain.team.project.controller;
 
+import com.nect.api.domain.team.project.dto.ProjectUserFieldReqDto;
+import com.nect.api.domain.team.project.dto.ProjectUserFieldResDto;
+import com.nect.api.domain.team.project.dto.ProjectUserResDto;
 import com.nect.api.domain.team.project.dto.UserProjectDto;
 import com.nect.api.domain.team.project.service.ProjectUserService;
 import com.nect.api.global.response.ApiResponse;
 import com.nect.api.global.security.UserDetailsImpl;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +27,20 @@ public class ProjectUserController {
             @AuthenticationPrincipal UserDetailsImpl user
     ) {
         return ApiResponse.ok(projectUserService.findProjectsByUser(user.getUserId()));
+    }
+
+    @PatchMapping("/{projectUserId}/field")
+    public ApiResponse<ProjectUserFieldResDto> updateProjectUserField(
+            @PathVariable @Positive Long projectUserId,
+            @RequestBody @Valid ProjectUserFieldReqDto reqDto
+    ) {
+        return ApiResponse.ok(projectUserService.changeProjectUserFieldInProject(projectUserId, reqDto));
+    }
+
+    @PatchMapping("/{projectUserId}/kick")
+    public ApiResponse<ProjectUserResDto> kickProjectUser(
+            @PathVariable @Positive Long projectUserId
+    ) {
+        return ApiResponse.ok(projectUserService.kickProjectUser(projectUserId));
     }
 }
--- nect-api/src/main/java/com/nect/api/domain/team/project/service/ProjectUserService.java
+++ nect-api/src/main/java/com/nect/api/domain/team/project/service/ProjectUserService.java
@@ -1,9 +1,14 @@
 package com.nect.api.domain.team.project.service;
 
 import com.nect.api.domain.team.project.converter.ProjectUserConverter;
+import com.nect.api.domain.team.project.dto.ProjectUserFieldReqDto;
+import com.nect.api.domain.team.project.dto.ProjectUserFieldResDto;
+import com.nect.api.domain.team.project.dto.ProjectUserResDto;
 import com.nect.api.domain.team.project.dto.UserProjectDto;
 import com.nect.api.domain.team.project.enums.code.ProjectErrorCode;
+import com.nect.api.domain.team.project.enums.code.ProjectUserErrorCode;
 import com.nect.api.domain.team.project.exception.ProjectException;
+import com.nect.api.domain.team.project.exception.ProjectUserException;
 import com.nect.core.entity.team.Project;
 import com.nect.core.entity.team.ProjectUser;
 import com.nect.core.entity.team.enums.ProjectMemberStatus;
@@ -12,6 +17,7 @@ import com.nect.core.entity.user.enums.RoleField;
 import com.nect.core.repository.team.ProjectUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,6 +26,30 @@ import java.util.List;
 public class ProjectUserService {
 
     private final ProjectUserRepository projectUserRepository;
+
+    @Transactional
+    public ProjectUserFieldResDto changeProjectUserFieldInProject(Long projectUserId, ProjectUserFieldReqDto reqDto) {
+        ProjectUser projectUser = getProjectUser(projectUserId);
+
+        if (reqDto.field() == RoleField.CUSTOM){
+            if (reqDto.customField() == null || reqDto.customField().isBlank()) {
+                throw new ProjectUserException(ProjectUserErrorCode.CUSTOM_FIELD_REQUIRED);
+            }
+        }
+
+        projectUser.changeField(reqDto.field(), reqDto.customField());
+        return ProjectUserFieldResDto.builder()
+                .projectUserId(projectUser.getId())
+                .field(projectUser.getRoleField())
+                .customField(projectUser.getCustomRoleFieldName())
+                .build();
+    }
+
+    private ProjectUser getProjectUser(Long projectUserId) {
+        ProjectUser projectUser = projectUserRepository.findById(projectUserId)
+                .orElseThrow(() -> new ProjectUserException(ProjectUserErrorCode.PROJECT_USER_NOT_FOUND));
+        return projectUser;
+    }
 
     public ProjectUser addProjectUser(Long userId, Project project, RoleField fieldId){
         ProjectUser projectUser = ProjectUser.builder()
@@ -52,5 +82,15 @@ public class ProjectUserService {
         return projectUsers.stream().
                 map(ProjectUserConverter::toUserProjectDto)
                 .toList();
+    }
+
+    public ProjectUserResDto kickProjectUser(Long projectUserId) {
+        ProjectUser projectUser = getProjectUser(projectUserId);
+
+        projectUser.kick();
+        return ProjectUserResDto.builder()
+                .id(projectUser.getId())
+                .memberStatus(projectUser.getMemberStatus())
+                .build();
     }
 }
