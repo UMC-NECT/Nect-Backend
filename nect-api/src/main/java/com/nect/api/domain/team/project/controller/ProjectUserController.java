299f2215d6480bd7677600a06aa5bba91fcd1730
zeoueon <zeoueon129@gmail.com>
2026-02-07 23:22:03 +0900

    [Refactor] 일부 API MypageController로 리팩토링

--- nect-api/src/main/java/com/nect/api/domain/team/project/controller/ProjectUserController.java
+++ nect-api/src/main/java/com/nect/api/domain/team/project/controller/ProjectUserController.java
@@ -1,14 +1,14 @@
 package com.nect.api.domain.team.project.controller;
 
-import com.nect.api.domain.team.project.dto.*;
+import com.nect.api.domain.team.project.dto.UserProjectDto;
 import com.nect.api.domain.team.project.service.ProjectUserService;
 import com.nect.api.global.response.ApiResponse;
 import com.nect.api.global.security.UserDetailsImpl;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -26,33 +26,4 @@ public class ProjectUserController {
         return ApiResponse.ok(projectUserService.findProjectsByUser(user.getUserId()));
     }
 
-    @PatchMapping("/{projectUserId}/field")
-    public ApiResponse<ProjectUserFieldResDto> updateProjectUserField(
-            @PathVariable @Positive Long projectUserId,
-            @RequestBody @Valid ProjectUserFieldReqDto reqDto
-    ) {
-        return ApiResponse.ok(projectUserService.changeProjectUserFieldInProject(projectUserId, reqDto));
-    }
-
-    @PatchMapping("/{projectUserId}/kick")
-    public ApiResponse<ProjectUserResDto> kickProjectUser(
-            @PathVariable @Positive Long projectUserId,
-            @AuthenticationPrincipal UserDetailsImpl user
-    ) {
-        return ApiResponse.ok(projectUserService.kickProjectUser(user.getUserId(), projectUserId));
-    }
-
-    @PatchMapping("/{projectUserId}/type")
-    public ApiResponse<ProjectUserResDto> updateProjectUserType(
-            @PathVariable @Positive Long projectUserId,
-            @RequestBody @Valid ProjectUserTypeReqDto req,
-            @AuthenticationPrincipal UserDetailsImpl user
-    ) {
-        return ApiResponse.ok(projectUserService.changeProjectUserTypeInProject(
-                user.getUserId(),
-                projectUserId,
-                req.memberType()
-            )
-        );
-    }
 }
